3a618e33898f89c0f09463cdc59e858de68f5d4e
kntjrld <68082685+kntjrld@users.noreply.github.com>
2022-11-11 16:16:27 +0800

    update

--- home.php
+++ home.php
@@ -8,110 +8,73 @@ if (isset($_SESSION['user_id']) && isset($_SESSION['user_name'])) {
 <html>
 
 <head>
-    <title>HOME</title>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
         integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/home_style.css">
 </head>
 
 <body>
-    <div class="pos-f-t">
-        <nav class="navbar navbar-dark bg-dark p-3">
-            <button class="navbar-toggler" type="button" data-toggle="collapse"
-                data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
-                aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <!-- <h6>Logged user: <?php echo $_SESSION['user_name']; ?></h6> -->
-            <a href="function/logout.php">Logout</a>
-        </nav>
+    <!-- Navigation start -->
+ <nav class="side-nav">
+    <ul>
+        <li>
+            <a href="#" class="logo">
+                <img src="media/logo.jpg" alt="logo">
+                <span class="nav-item">XYZ</span>
+            </a>
+        </li>
+        <li><a href="#">
+        <i class="fas fa-home"></i>
+        <span class="nav-item">Dashboard</span>
+        </a></li>
+        <li><a href="#">
+        <i class="fas fa-table"></i>
+        <span class="nav-item">Records</span>
+        </a></li>
+        <li><a href="#">
+        <i class="fas fa-tasks"></i>
+        <span class="nav-item">Reports</span>
+        </a></li>
+        <li><a href="#">
+        <i class="fas fa-user"></i>
+        <span class="nav-item">Users</span>
+        </a></li>
+        <li><a href="#">
+        <i class="fas fa-solid fa-clock-rotate-left"></i>
+        <span class="nav-item">Activities</span>
+        </a></li>
+        <li><a href="function/logout.php" class="logout">
+        <i class="fas fa-sign-out-alt"></i>
+        <span class="nav-item">Logout</span>
+        </a></li>
+    </ul>
+ </nav> 
+   <!-- Navigation end -->  
+    <div class="main-content">
+        <header>
+            <div class="header-title">
+                <h2>
+                    <label for="">
+                    <span class="las la-bars">
+                    </span>
+                    Dashboard
+                </h2>
+            </div>
+            <div class="user-wrapper">
+                <img src="media/male.png" width="30px" height="30px" alt="user">
+                <div>
+                    <h5><span class="nav-item"><?php echo $_SESSION['user_name']; ?></span></h5>
+                </div>
+            </div>
+            
+        </header>
     </div>
-    <!-- <div class="d-flex justify-content-between" id="header">
-        <h6 class="justify-content-start">Logged user: <?php echo $_SESSION['user_name']; ?></h6>c
-        <a href="function/logout.php">Logout</a>
-    </div> -->
-    <h6 class="justify-content-start p-3">Logged user: <?php echo $_SESSION['user_name']; ?></h6>
-    <!-- <h6>Email: <?php echo $_SESSION['email']; ?></h6> -->
-    <div class="container my-2">
-        <nav class="navbar navbar-light bg-light justify-content-start">
-            <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            </form>
-        </nav>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>E-mail</th>
-                    <th>Title</th>
-                    <th>Department</th>
-                    <th>Status</th>
-                    <th>Position</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>John Doe</td>
-                    <td>john.doe@gmail.com</td>
-                    <td>Software engineer</td>
-                    <td>IT department</td>
-                    <td>Active</td>
-                    <td>Senior</td>
-                    <td>
-                        <button type="button" class="btn btn-primary btn-sm">
-                            Edit
-                        </button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Alex Ray</td>
-                    <td>alex.ray@gmail.com</td>
-                    <td>Consultant</td>
-                    <td>Finance</td>
-                    <td>Onboarding</td>
-                    <td>Junior</td>
-                    <td>
-                        <button type="button" class="btn btn-primary btn-sm">
-                            Edit
-                        </button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Kate Hunington</td>
-                    <td>kate.hunington@gmail.com</td>
-                    <td>Designer</td>
-                    <td>UI/UX</td>
-                    <td>Awaiting</td>
-                    <td>Senior</td>
-                    <td>
-                        <button type="button" class="btn btn-primary btn-sm">
-                            Edit
-                        </button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-
-    <nav aria-label="Page navigation example">
-        <ul class="pagination justify-content-center">
-            <li class="page-item disabled">
-                <a class="page-link" href="#" tabindex="-1">Previous</a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item">
-                <a class="page-link" href="#">Next</a>
-            </li>
-        </ul>
-    </nav>
 </body>
-
 </html>
-
 <?php 
+
 }else{
      header("Location: index.php");
      exit();
